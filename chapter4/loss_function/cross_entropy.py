fe0bae15ddf886200ef872d4a86307ae761db877
Shoma Kobayashi <sho1228.k@gmail.com>
2022-06-30 21:52:42 +0900

    update: 交差エントロピー関数をバッチ処理に対応させる

--- chapter4/loss_function/cross_entropy.py
+++ chapter4/loss_function/cross_entropy.py
@@ -1,12 +1,22 @@
 import numpy as np
 
 def cross_entropy_error(y, t):
+	# yの次元が1だった場合
+	if y.ndim == 1:
+		# 2次元の配列に変換する
+		t = t.reshape(1, t.size)
+		y = y.reshape(1, y.size)
+
+	batch_size = y.shape[0] # 要素数
 	delta = 1e-7 # -∞を発生しないための対策
-	return -np.sum(t * np.log(y+delta))
+	# one-hot 表現ではなく、「2」などがラベルとして与えられる場合
+	# arange: 連続した配列の自動作成
+	# ここの詳しい解説はp95
+	return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
 
 
 # 2を正解とする
-t = [0,0,1,0,0,0,0,0,0,0]
+t = 2
 
 # 2の確率が最も高い場合
 y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
